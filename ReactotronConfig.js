d768b88495b5637a8b6f7edc9949ee0ddfe3885e
Riqki Kamal Amrela <riqkikamal@outlook.com>
2020-09-06 19:40:57 +0700

    Update ReactotronConfig.js

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,14 +1,17 @@
 import Reactotron from 'reactotron-react-native';
-import {AsyncStorage} from '@react-native-community/async-storage';
 
-const middleware = (tron) => {
-  /* plugin definition */
+const yeOldeConsoleLog = console.log;
+
+console.log = (...args) => {
+  yeOldeConsoleLog(...args);
+  Reactotron.display({
+    name: 'CONSOLE.LOG',
+    value: args,
+    preview: args.length > 0 && typeof args[0] === 'string' ? args[0] : null,
+  });
 };
-
-Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-  .configure({
-    name: 'React Native Demo',
-  })
+Reactotron.configure({
+  name: 'myDoctor',
+}) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
-  .use() // plus some custom made plugin.
   .connect();
